e22548eb654d10f4023847711fa5f6d8d84b7be2
gaelitogamer2022-ship-it <gaelito.gamer.2022@gmail.com>
2026-02-24 12:54:37 -0500

    Create aplicación1.py

---
+++ aplicación1.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cómo te llamas?")
+edad = int(input("¿Cuántos años tienes?"))
+# convertir la entrada del usuario a un número entero
+
+edad_siguiente_ano = edad + 1
+
+print("Hola,", nombre, "¡en un año tendrás", edad_siguiente_ano, "años!")
