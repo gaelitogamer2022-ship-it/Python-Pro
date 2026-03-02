0657f0c811f7ad8db640416c821cd4e40a1265f1
gaelitogamer2022-ship-it <gaelito.gamer.2022@gmail.com>
2026-03-02 10:50:36 -0500

    Delete aplicación1.py

--- aplicación1.py
+++
@@ -1,6 +0,0 @@
-nombre = input("¿Cómo te llamas?")
-edad = int(input("¿Cuántos años tienes?"))
-
-edad_siguiente_ano = edad + 1
-
-print("Hola,", nombre, "¡en un año tendrás", edad_siguiente_ano, "años!")
